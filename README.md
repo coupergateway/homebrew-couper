0d4a743e554de3133e9e965e808896c9eaf7f4d1
Marcel Ludwig <marcel.ludwig@milecrew.com>
2023-12-01 13:26:55 +0100

    update repository

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This repository is a Homebrew Tap for [couper](https://couper.io/).
 
 ## How do I install these formulae?
 
-`brew tap avenga/couper` and then `brew install couper`.
+`brew tap coupergateway/couper` and then `brew install couper`.
--- couper.rb
+++ couper.rb
@@ -3,11 +3,11 @@ class Couper < Formula
     homepage "https://couper.io/"
     license "MIT"
     version "v1.12.1"
-    head "https://github.com/avenga/couper.git", branch: "master"
+    head "https://github.com/coupergateway/couper.git", branch: "master"
 
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/avenga/couper/releases/download/v1.12.1/couper-v1.12.1-macos-arm64.zip"
+        url "https://github.com/coupergateway/couper/releases/download/v1.12.1/couper-v1.12.1-macos-arm64.zip"
         sha256 "46ceb086808ed3cfa06d6af9f7eb5147219a6aa4a4fda714eaed9ef1f6caaabc"
 
         def install
@@ -15,7 +15,7 @@ class Couper < Formula
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/avenga/couper/releases/download/v1.12.1/couper-v1.12.1-macos-amd64.zip"
+        url "https://github.com/coupergateway/couper/releases/download/v1.12.1/couper-v1.12.1-macos-amd64.zip"
         sha256 "793d998f3ebd4d75ff4a4dab7e8562941cf191e0af80acd0cd1e4844ce3ae212"
 
         def install
@@ -26,7 +26,7 @@ class Couper < Formula
 
     on_linux do
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/avenga/couper/releases/download/v1.12.1/couper-v1.12.1-linux-arm64.tar.gz"
+        url "https://github.com/coupergateway/couper/releases/download/v1.12.1/couper-v1.12.1-linux-arm64.tar.gz"
         sha256 "a8a1ad3cd3b35092965d709ab5d20ac67c613c159dee27abb6e6efdfa8e8116f"
 
         def install
@@ -34,7 +34,7 @@ class Couper < Formula
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/avenga/couper/releases/download/v1.12.1/couper-v1.12.1-linux-amd64.tar.gz"
+        url "https://github.com/coupergateway/couper/releases/download/v1.12.1/couper-v1.12.1-linux-amd64.tar.gz"
         sha256 "f799f100e99a4ed2a3aadf570671874c0f6348a7ed994b97c90a01f120a5b78d"
 
         def install
