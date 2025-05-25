dbb7a1ea46c45576047c40d408610eabbad36307
Marcel Ludwig <mail@marcel.dev>
2025-05-25 15:07:16 +0200

    Update formula to v1.13.1 release (#17)
    Co-authored-by: couper-bot <couperbot@gmail.com>

--- couper.rb
+++ couper.rb
@@ -2,21 +2,21 @@ class Couper < Formula
     desc "Couper is a lightweight open source API gateway designed to support developers in building and running API-driven Web projects."
     homepage "https://couper.io/"
     license "MIT"
-    version "v1.13.0"
+    version "v1.13.1"
     head "https://github.com/coupergateway/couper.git", branch: "master"
 
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/coupergateway/couper/releases/download/v1.13.0/couper-v1.13.0-macos-arm64.zip"
-        sha256 "d993b6d55b196e280e4facd71f1ef093f3e08681f541da38f501d005f5167c1d"
+        url "https://github.com/coupergateway/couper/releases/download/v1.13.1/couper-v1.13.1-macos-arm64.zip"
+        sha256 "0fa2d1efeeacc8f6f970d1e4d72b25e97078f9e6c83a8cc117cad52cf55eeca0"
 
         def install
           bin.install "couper"
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/coupergateway/couper/releases/download/v1.13.0/couper-v1.13.0-macos-amd64.zip"
-        sha256 "588ccc077ce23aaf42b59f4c2d546275a340b615c370144fd4d134ca0f42f0c8"
+        url "https://github.com/coupergateway/couper/releases/download/v1.13.1/couper-v1.13.1-macos-amd64.zip"
+        sha256 "f93aee72b66ba095ef1f9ca717d7bdfbb4f974eec4224f7392ebcc8581bcb60e"
 
         def install
           bin.install "couper"
@@ -26,7 +26,7 @@ class Couper < Formula
 
     on_linux do
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-        url "https://github.com/coupergateway/couper/releases/download/v1.13.0/couper-v1.13.0-linux-arm64.tar.gz"
+        url "https://github.com/coupergateway/couper/releases/download/v1.13.1/couper-v1.13.1-linux-arm64.tar.gz"
         sha256 ""
 
         def install
@@ -34,7 +34,7 @@ class Couper < Formula
         end
       end
       if Hardware::CPU.intel?
-        url "https://github.com/coupergateway/couper/releases/download/v1.13.0/couper-v1.13.0-linux-amd64.tar.gz"
+        url "https://github.com/coupergateway/couper/releases/download/v1.13.1/couper-v1.13.1-linux-amd64.tar.gz"
         sha256 ""
 
         def install
