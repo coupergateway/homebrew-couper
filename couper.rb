2483f2bd3c1d5179fcefb6ce45faaca281361506
malud <mail@marcel.dev>
2026-09-16 16:01:54 +0200

    Update formula to v1.14.3 release

--- couper.rb
+++ couper.rb
@@ -2,9 +2,9 @@ class Couper < Formula
     desc "Couper is a lightweight open source API gateway designed to support developers in building and running API-driven Web projects."
     homepage "https://couper.io/"
     license "MIT"
-    version "v1.14.2"
-    url "https://github.com/coupergateway/couper/archive/refs/tags/v1.14.2.tar.gz"
-    sha256 "a50b3e3e10fce720dd9158d771f8651d4f64cdf9d0e70fb3b0c997812dba7f4b"
+    version "v1.14.3"
+    url "https://github.com/coupergateway/couper/archive/refs/tags/v1.14.3.tar.gz"
+    sha256 "f88e8196d254e3dfd381f2d14c57237d63fc47498e7b5d3858a1a7d3dc9e5a6b"
     head "https://github.com/coupergateway/couper.git", branch: "main"
 
     depends_on "go" => :build
@@ -18,7 +18,7 @@ class Couper < Formula
         end
       ldflags = %W[
         -X github.com/coupergateway/couper/utils.VersionName=#{version}
-        -X github.com/coupergateway/couper/utils.BuildName=d85f6fb
+        -X github.com/coupergateway/couper/utils.BuildName=4ca0547
         -X github.com/coupergateway/couper/utils.BuildDate=#{build_time_str}
       ]
       system "go", "build", *std_go_args(ldflags:)
